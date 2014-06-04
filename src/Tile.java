45c241635b88756c9256683fc74ea94b75dec816
Mike <minghan.mike.li@gmail.com>
2014-06-04 11:21:25 -0400

    Made tile class

---
+++ src/Tile.java
@@ -0,0 +1,21 @@
+import java.awt.Image;
+
+
+public class Tile {
+
+private Image displaypic;
+
+public Tile(int tileid)
+{
+	
+}
+
+public Image getDisplaypic() {
+	return displaypic;
+}
+
+public void setDisplaypic(Image displaypic) {
+	this.displaypic = displaypic;
+}
+
+}
